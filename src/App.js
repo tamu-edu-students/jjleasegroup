adf5dd315121c1c803b469a5f2d08be0919ede04
Winston0323 <zil004@ucsd.edu>
2022-10-19 16:24:52 -0500

    finished static login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 function App() {
+  const adminUser= {
+    email:"admin@admin.com",
+    password: "admin"
+
+  }
+  const [user, setUser] = useState({name:"", email:""});
+  const [error, setError] = useState("");
+
+  const checkLogin = details =>{
+
+    //testing email and password matching
+    if (details.email === adminUser.email && details.password === adminUser.password){
+      console.log("Admin login");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    }else{// password wrong
+
+      if(details.password !== adminUser.password){ 
+        setError("Passward wrong")
+      }else{
+        setError("Detail wrong")
+      }
+      
+      console.log("Detail do not match");
+    }
+  }
+
+  const Logout = () => {
+
+    setUser({name:"", email:""});
+    console.log("Logout");
+  
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !== "") ? (//when succefully login 
+        
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      
+      ):(//when havent log in, call loginform page
+        
+        <LoginForm checkLogin={checkLogin} error={error}/>
+      
+      )}
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+
+function LoginForm({ checkLogin , error}){
+    const [details, setDetails] = useState({name: "", email: "", password:""});
+    
+    //pass through detail 
+    const submitHandler = e =>{
+        e.preventDefault();
+        checkLogin(details);
+    }
+
+    return(
+        <form onSubmit={submitHandler}>  
+            <div className='form-inner'>
+                <h2>Login</h2>
+                
+                <div className='form-group'>
+                    <label htmlFor='name'>Name:</label>
+                    <input type="text" name="name" id="name" onChange={e=>setDetails({...details, name: e.target.value})} value = {details.name} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="emai" id="email" onChange={e=>setDetails({...details, email: e.target.value})} value = {details.email}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">password: </label>
+                    <input type="password" name="password" id="password" onChange={e=>setDetails({...details, password: e.target.value})} value = {details.password}/>
+                </div>
+                <input type="submit" value="LOGIN" />
+                {/*ERROR*/error !=="" ? (<div className='error'>{error}</div>) : "" }
+            </div>
+        </form>
+    )
+
+}
+
+export default LoginForm
