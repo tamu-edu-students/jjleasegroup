6da34f0542807ca1e4b1466b9b8da58d188b8b55
Leonora Liu <junru.liu@tamu.edu>
2022-10-20 11:23:00 -0500

    sign_up-update_info-delete_account

--- apartment_finder_app/urls.py
+++ apartment_finder_app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from apartment_finder_app import views
 
 urlpatterns = [
-    url(r'^question_consultation$', views.question_consultation_API, name='add-question-consultation'),
-    url(r'^question_consultation/([0-9]+)$', views.question_consultation_API, name='retrieve_question_consultation')
+    url(r'^question_consultation$', views.question_consultation_API, name='add_question_consultation'),
+    url(r'^question_consultation/([0-9]+)$', views.question_consultation_API, name='retrieve_question_consultation'),
+    url(r'^customer$', views.customer_API, name='customer'),
+    url(r'^customer/([0-9]+)$', views.customer_API, name='remove_customer')
 ]
