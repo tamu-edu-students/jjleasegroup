e5e2fb194a581ce36e27a3e1551b40f10432ab6a
LucasLyu <lvxl595@gmail.com>
2022-11-12 18:18:53 -0600

    feat(frontend: component): add some customized css

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -2,24 +2,26 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import styles from "./styles.module.scss";
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className={styles.nav} expand="lg">
       <Container>
-        <Navbar.Brand href="#home">JJ Group</Navbar.Brand>
+        <Navbar.Brand className={styles.brand} href="/">
+          JJtxhome
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse className={styles.collapse}>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Contact Us</Nav.Link>
-            <Nav.Link href="#link">FAQ</Nav.Link>
-            <NavDropdown title="Leasing" id="basic-nav-dropdown">
+            <NavDropdown title="Leasing">
               <NavDropdown.Item href="/Austin">Austin</NavDropdown.Item>
               <NavDropdown.Item href="/CollegeStation">
                 College Station
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="/ContactUs">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
