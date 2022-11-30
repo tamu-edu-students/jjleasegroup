4950958f55705e0a99b4f0933456c0b40185e2e6
KforKelvin <ky1015@tamu.edu>
2022-11-30 12:00:54 -0600

    Update settings.py

--- apartment_finder/settings.py
+++ apartment_finder/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'apartment_finder',
         'USER': 'root',
-        'PASSWORD': '!YYmm186014',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
