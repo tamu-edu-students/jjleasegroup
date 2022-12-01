8ad6d794ab68e30d9bdd12e5fad36896c2e52e01
root <root@ip-172-31-88-140.ec2.internal>
2022-12-01 01:46:24 +0000

    2022/11/30: django settings idk

--- apartment_finder/settings.py
+++ apartment_finder/settings.py
@@ -138,7 +138,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-SECURE_SSL_REDIRECT = False
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
 
 
 
