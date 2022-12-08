9bc1564308bbb8ad3258e07634eb6896aca6cc32
KforKelvin <ky1015@tamu.edu>
2022-12-08 15:02:54 -0600

    Update APIService.js

--- frontend/src/api/APIService.js
+++ frontend/src/api/APIService.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://jjgroup-django.herokuapp.com/";
-const baseUrl = "http://127.0.0.1:8000/";
+const baseUrl = "https://jjgroup-django.herokuapp.com/";
+// const baseUrl = "http://127.0.0.1:8000/";
 
 
 export default class APIService {
