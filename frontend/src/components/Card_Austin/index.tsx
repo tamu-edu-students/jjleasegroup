3ed56cb1e3256d10f2a00ac295587b6ac2793aa7
KforKelvin <ky1015@tamu.edu>
2022-12-07 20:22:46 -0600

    tag displaying not correct

--- frontend/src/components/Card_Austin/index.tsx
+++ frontend/src/components/Card_Austin/index.tsx
@@ -1,14 +1,13 @@
 import APIService from "../../api/APIService";
 import styles from "./styles.module.scss";
 import React, { useState,useEffect } from "react"
+import { render } from "@testing-library/react";
 
 
 
 
 
-const Card_Austin = () =>{
-
-
+function Card_Austin() {
     const [results, setResults] = useState([{ 
         id: "1", name: "test", 
         city: "0", 
@@ -31,6 +30,7 @@ const Card_Austin = () =>{
 
             for(let i = 0; i< resp.length; i++ ){
                 if (resp[i].apt_city === 1){
+                    
                     setResults(results => [...results, {
                         id: resp[i].apt_id,
                         name: resp[i].apt_name,
@@ -39,71 +39,87 @@ const Card_Austin = () =>{
                         zipcode:  resp[i].apt_zipcode,
                         price_low:  resp[i].apt_price_low,
                         price_high: resp[i].apt_price_high,
-                        near_campus:resp[i].apt_near_campus,
+
+                        near_campus: resp[i].apt_near_campus,
                         furnished:  resp[i].apt_furnished,
                         free_parking: resp[i].apt_free_parking,
                         free_we:  resp[i].apt_free_we,
                         free_internet: resp[i].apt_free_internet,
+                        
                         url: resp[i].apt_url,
                         picture_url: resp[i].apt_picture_url
-                        }]);
+                    }]);
                 }
+                
             }
         });
     }
+
+
     useEffect(() => {
         aptInfoGetter();
       }, []);
    
-    const renderApt = results.slice(1+(results.length/2)).map((Apt) =>  
-        <div className={styles.card} key={Apt.id} >
-            <div className={styles["card-header"]}>
+    
+    const renderApt = results.slice(1+(results.length/2)).map((Apt) => 
+     
+        // let a = {Apt.near_campus};
+        <div className={styles.card}>
+            {Apt.near_campus}
+            <div className={styles["card-header"]} key={Apt.id}>
                 <img className={styles["card-image"]} src={Apt.picture_url}/>
             </div>
         
             <div className={styles.name}>
                 {Apt.name}
             </div>
-        
-            <div className={styles.advants}>
-                <div>
-                    {/* <span>price:</span> */}
-                    <div  className={styles.price}><span>${Apt.price_low} - {Apt.price_high}</span></div>
-                </div>
-
-                <div className={styles.address}>
-                    {Apt.street}, Austin, Texas, {Apt.zipcode}
-                </div>
-
-                {/* <div className={styles.margin}>
-                    <div className={styles.TagGrid}>
-                        <div className={styles.tag}>Near Campus</div>
-                        <div className={styles.tag}>Furnished</div>
-                    </div>
-                    <div className={styles.TagGrid}>
-                        <div className={styles.tag}>Free Parking</div>
-                        <div className={styles.tag}>Free Uiltility</div>
-                    </div>
-                    <div className={styles.tag}>Free wifi</div>
-                </div> */}
             
-            </div>
+                    
+            <div  className={styles.price}><span>${Apt.price_low} - {Apt.price_high}</span></div>
         
-            <div className={styles.footer}>
-                <button className={styles.btn}>Contact Us</button>
-                <button className={styles["btn-outline"]}><a href={Apt.url}>More Info</a></button>
+
+            <div className={styles.address}>
+                {Apt.street}, Austin, Texas, {Apt.zipcode}
+            </div>
+
+            
+            <button className={styles.btn}>Contact Us</button>
+            <button className={styles["btn-outline"]}><a href={Apt.url}>More Info</a></button>
+
+            <div className={styles.footer}>   
+                <div className={styles.TagGrid}>
+                
+                    <div className={styles.tag}>Near Campus: {Apt.near_campus ? "Yes": "No"}</div>
+                    <div className={styles.tag}>Furnished: {Apt.furnished ? "Yes": "No"}</div>
+                    {/* {Apt.near_campus 
+                            ? <div className={styles.tag}>Near Campus</div> 
+                            : <div></div>} </div> */}
+                </div>    
+                
+                <div className={styles.TagGrid}>
+                    <div className={styles.tag}>Free Parking: {Apt.free_parking ? "Yes":"No"} </div>
+                    <div className={styles.tag}>Free Uiltility: {Apt.free_parking ? "Yes":"No"}</div>
+                </div>
+                <div className={styles.TagGrid}>
+                    <div className={styles.tag}>Free wifi: {Apt.free_parking ? "Yes":"No"}</div>    
+                </div>    
+                
             </div>
         </div>  
     );
-
     
+
     return (
        
         <div className={styles.grid}>
             {renderApt}
+            {/* {test[1].name} */}
         </div>
         
     );
+
+
+
    
     
     
